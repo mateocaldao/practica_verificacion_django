0b077f68c717744cdde6c83429ce0893ac1c675b
Mateo Cerviño Aldao <mateo3aldao@MacBook-Pro-de-Mateo.local>
2016-05-07 12:23:08 +0200

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mateo3aldao/Desktop/U-TAD/Curso 15-16/CUATRIMESTRE 2/VERIFICACION Y DESARROLLO DE PROGRAMAS/practica_verificacion_django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
