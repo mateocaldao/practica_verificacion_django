60526e1556d0012127866c7fc0db22e5a2581081
Mateo Cerviño Aldao <mateo3aldao@MacBook-Pro-de-Mateo.local>
2016-05-22 19:38:01 +0200

    Added some model tests in tests.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,7 @@
 import datetime
 from django.utils import timezone
 from django.db import models
-from django.math import sum
+
 
 
 
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -17,6 +17,59 @@ class QuestionModelTest(TestCase):
         poll.question_text = "Shall we begin?"
         poll.pub_date = timezone.now()
         poll.save()
-        
+
+
+    def test_total_votes_poll(self):
+
+        #creating a poll object
+        p = Question(question_text='Can I vote?', pub_date=timezone.now())
+        p.save()
+        self.assertTrue(p.total_votes(),0)
+
+
+class ChoiceModelTest(TestCase):
+    def test_creating_new_choice_and_saving_it_to_the_database(self):
+
+        #creating a poll object
+        poll = Question(question_text='What are my choices?', pub_date=timezone.now())
+        poll.save()
+
+        #creating a choice object
+        choice = Choice(question=poll, choice_text='You have no choice', choice_votes=0)
+        choice.save();
+
+        #searching in database
+        all_choices_in_database = poll.choice_set.all()
+        only_choice_in_database = all_choices_in_database[0]
+
+        self.assertEquals(only_choice_in_database,"You have no choice")
+
+    def test_diferent_number_of_votes_in_question_and_choice(self):
+
+        #creating a poll object
+        p = Question(question_text='How are you?', pub_date=timezone.now())
+        p.save()
+
+        #creating first choice
+        c1 = Choice(question=p, choice_text='Fine', choice_votes=2)
+        c1.save()
+
+        #creating second choice
+        c2 = Choice(question=p, choice_text='Cool', choice_votes=4)
+        c2.save()
+
+        #correct number of choices in question
+        number_of_votes = p.total_votes()
+
+        self.assertNotEquals(number_of_votes,c1.votes())
+
+
+
+
+
+
+
+
+
 
 
